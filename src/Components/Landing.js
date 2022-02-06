9a249c8cb4c9475c0a1ce19f4041c82aa351475a
amcmullen <amcmullen9291@gmail.com>
2022-02-05 20:59:38 -0500

    onInput w/ autocomplete off

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Landing() {
+
+  let [ translation, setTranslation ] = useState(null);
+
+
+  function findTranslation(e){
+    e.preventDefault();
+    let enteredExpression = document.getElementById("inputArea").value;
+    document.getElementById('RegexTranslated').innerHTML = enteredExpression;
+    setTranslation(enteredExpression);
+}
+
   return (
       <>
   <div>
     <center><h1>Landing Page</h1></center>
   </div>
+<center>
+  <form>
+    <input id="inputArea" type="text" onInput={(e) => {findTranslation(e)}} placeholder="enter Regex" autoComplete='off'/>
+  </form>
+</center>
+<br/>
+<hr/>
+  <center><div id="RegexTranslated">
+
+  </div></center>
+<br/>
+  {translation}
   </>
   );
 }
