47d7f2e4de6fb3da8bf6f2d15d864317ee9af883
amcmullen <amcmullen9291@gmail.com>
2022-02-05 22:57:45 -0500

    regex constants for HTML

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -4,32 +4,52 @@ import { useState } from 'react';
 export default function Landing() {
 
   let [ translation, setTranslation ] = useState(null);
+  const backslash = String.raw` \ `;
 
+  const charClasses = [ backslash, ", ", ".", " , ", "\\cX", ", ", "\\d", ", ", "\\D", ", ", "\\f", ", ", "\\n",", ", "\\r",", ", "\\s",", ", "\\S",", ", "\\t",", ", "\\v",", ", "\\w",", ", "\\W",", ", "\\0",", ", "[^]"];
+  const assertions = ["^", ", ", "$", ", ",  "\\b", ", ",  "\\B"]
+  const quantifiers = ["*", ", ", "+", ", ", "?", ", ", "x{n}", ", ", "x{n,}", ", ", "x{n,m}"];
+  //for .includes() method
+
+  const HTMLCharClasses = [ < span key={"1"}>{backslash}</span>]
+  const HTMLAsserstions = [];
+  const HTMLQuantifiers = [];
+  //@ mapping HTMLClasses for nicer visual HTML
 
   function findTranslation(e){
     e.preventDefault();
     let enteredExpression = document.getElementById("inputArea").value;
     document.getElementById('RegexTranslated').innerHTML = enteredExpression;
     setTranslation(enteredExpression);
+    console.log(enteredExpression[enteredExpression.length -1]);
+    //reads last input from translate
 }
 
   return (
       <>
   <div>
-    <center><h1>Landing Page</h1></center>
+    <h1>Enter an Expression</h1>
   </div>
 <center>
   <form>
-    <input id="inputArea" type="text" onInput={(e) => {findTranslation(e)}} placeholder="enter Regex" autoComplete='off'/>
+    <input id="inputArea" type="text" onInput={(e) => {findTranslation(e)}} placeholder="Regular Expressions" autoComplete='off'/>
   </form>
 </center>
 <br/>
-<hr/>
+<div id="charClass">
+  <span className="regexHeading"><center>Charater Class: </center></span><center>{charClasses}</center>
+  <span className="regexHeading"><center>Assertions: </center></span><center>{assertions}</center>
+  <span className="regexHeading"><center>Quantifiers: </center></span><center>{quantifiers}</center>
+</div>
+
   <center><div id="RegexTranslated">
 
   </div></center>
-<br/>
+<center><br/><br/><br/>
+ <div id="translatedArea">
   {translation}
+  </div>
+  </center>
   </>
   );
 }
