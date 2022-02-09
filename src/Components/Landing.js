01eb745c0328308b1909251fe220838e9a518b18
amcmullen <amcmullen9291@gmail.com>
2022-02-08 20:16:23 -0500

    safety commit

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -12,8 +12,7 @@ export default function Landing() {
 
   const charClasses = [ "", "[x|y]", "." , "", "\\d", "\\D", "", "\\n", "\\r", "\\s", "\\S", "\\t", "", "\\w", "\\W", "\\0", "[^]", "[x-y]"];
   const assertions = ["^", "$", "\\b", "\\B", "|" ]
-  const quantifiers = ["*", "+", "?", "x{n}", "x{n,}", "x{n,m}"];
-  //for .includes() method
+  const quantifiers = ["*", "+", "?", "{n}", "{n,}", "{n,m}"];
 
   const definitionsHTMLCharClass = ["placeholder" , "Matches the first occurance of either term 'x' or term 'y'.", "A wildcard; this matches any single character. However, inside square brackets, it matches a literal dot or period.", "", "This expression will match any digit. It is equivalent to the Character Case expression [0-9].", "This expression matches any character that is not a digit. It is equivalent to Character Case expression [^0-9].", "", "This expression looks for the character used to signify the end of a line of text and the start of a new line which is ' \\n '.", "This expression looks for the control character used to reset a cursor's position to the beginning of a line of text (like when you use the return key)."];
   const definitionsAssertions = ["placeholder", "Outside of square brackets, a caret looks for matches at the beginning of a string. Inside square brackets, a caret matches values NOT equal to the given filter."];
@@ -30,27 +29,18 @@ export default function Landing() {
   function findTranslation(e){
     e.preventDefault();
     let enteredExpression = document.getElementById("inputArea").value;
-    console.log( "current translation value: ", translation);
     if(translation != null){
       if(enteredExpression.length < inputLength){
         if((enteredExpression.length < 1) && (enteredExpression.length > -1) ){
-          console.log("right area");
-          // setTranslation(); 
           setTranslation(translation.slice(0, -1));
           setInputLength(0);
         }else{
-          // setTranslation();
         setTranslation(translation.slice(0, -1));
         setInputLength(enteredExpression.length);
-        console.log(enteredExpression.length);
-        console.log("input length: ", enteredExpression.length);
       }
       }else{
         setTranslation(translation += enteredExpression[enteredExpression.length-1]);
         setInputLength(inputLength+=1);
-        console.log(enteredExpression);
-        console.log("input length: ", inputLength);
-
       }
     }
     if(translation == null){
@@ -118,8 +108,12 @@ function define2 (e, index){  //Assertions definitions
   let enteredExpression = assertions[index-1];
   if(translation != null){
     setTranslation(translation + enteredExpression);
+    document.getElementById("inputArea").value ="";
+    setInputLength(0);
   }else{
     setTranslation(enteredExpression);
+    document.getElementById("inputArea").value ="";
+    setInputLength(0);
   }
 }
 
@@ -130,8 +124,12 @@ function define3 (e, index){  //quantifiers definitions
   let enteredExpression = quantifiers[index-1];
   if(translation != null){
     setTranslation(translation + enteredExpression);
+    document.getElementById("inputArea").value ="";
+    setInputLength(0);
   }else{
     setTranslation(enteredExpression);
+    document.getElementById("inputArea").value ="";
+    setInputLength(0);
   }
 }
 
@@ -145,14 +143,6 @@ function readInputIndexbyIndex(){  //not needed
       console.log(enteredExpression[enteredExpression.length -1]);
   }
 }
- //something like
- //let searchterm = "";  (need to set constant)
- //while((!charClasses.includes(enteredExpression[enteredExpression.length -1])) && (!quantifers.includes(enteredExpression[enteredExpression.length -1]) && (!charClasses.includes(enteredExpression[enteredExpression.length -1])){
-//    searchTerm += enteredExpression[enteredExpression.length -1])
-// }
-//... and if it does contain an expression, set serach term  (searchterm, setSearchterm)
-//also need to set the "n"s in the constants to accept any number
-
 
   return (
       <>
