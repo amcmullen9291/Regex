2537611d302a902eb6d2b3da81516788ce8b3251
amcmullen <amcmullen9291@gmail.com>
2022-02-08 19:01:14 -0500

    dynamically create expression

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -9,7 +9,7 @@ export default function Landing() {
 
   const backslash = String.raw` \ `;
 
-  const charClasses = [ backslash, ".", "\\d", "\\D", "\\n", "\\r", "\\s", "\\S", "\\t", "\\w", "\\W", "\\0", "[^]"];
+  const charClasses = [ "", "[x|y]", "." , "", "\\d", "\\D", "", "\\n", "\\r", "\\s", "\\S", "\\t", "", "\\w", "\\W", "\\0", "[^]", "[x-y]"];
   const assertions = ["^", "$", "\\b", "\\B", "|" ]
   const quantifiers = ["*", "+", "?", "x{n}", "x{n,}", "x{n,m}"];
   //for .includes() method
@@ -18,9 +18,9 @@ export default function Landing() {
   const definitionsAssertions = ["placeholder", "Outside of square brackets, a caret looks for matches at the beginning of a string. Inside square brackets, a caret matches values NOT equal to the given filter."];
   const definitionsQuantifiers = [ "placeholder" , "The asterisk will look for the preceding character 0 or more times. Note: if multiple letters are searched for, ALL of the letters will have to appear in the searched string in the same order as the entered regex filter value. The asterisk only affects the character immediately before it.","","","","","","The 'g' modifier specifies a global match. It will find all matches present in a string." ];
 
-  const HTMLCharClasses = [ < span key={"2"} onClick={(e) => {define(e, "2")}}>. </span>, <span>, </span>, < span key={"4"} onClick={(e) => {define(e, "4")}}>\d </span>,<span>, </span>, < span key={"5"} onClick={(e) => {define(e, "5")}}>\D </span>,<span>, </span>, < span key={"7"} onClick={(e) => {define(e, "7")}}>\n </span>,<span>, </span>, < span key={"8"} onClick={(e) => {define(e, "8")}}>\r </span>,<span>, </span>, < span key={"9"}>\s </span>,<span>, </span>, < span key={"10"}>\S </span>,<span>, </span>, < span key={"11"}>\t </span>,<span>, </span>, < span key={"13"}>\w </span>,<span>, </span>, < span key={"14"}>\W </span>,<span>, </span>, < span key={"15"}>\0 </span>,<span>, </span>, < span key={"16"}>[^  ]</span>, <span>,</span>,< span key={"17"}>[x-y]</span> , <span>, </span>, < span key={"1"} onClick={(e) => {define(e, "1")}}>[x|y]</span>]
-  const HTMLAssertions = [< span key={"1"} onClick={(e) => {define2(e, "1")}}>^</span>, <span>, </span>,< span key={"2"}>$</span>, <span>, </span>,< span key={"3"}>\b</span>, <span>, </span>,< span key={"4"}>\B</span> ];
-  const HTMLQuantifiers = [< span key={"1"} onClick={(e) => {define3(e, "1")}}>*</span>, <span>, </span>,< span key={"2"}>+</span>, <span>, </span>,< span key={"3"}>?</span>, <span>, </span>,< span key={"4"}>x&#x007B;n&#x007D;</span>, <span>, </span>,< span key={"5"}>x&#x007B;n,_&#x007D;</span>, <span>, </span>,< span key={"6"}>x&#x007B;n,m&#x007D;</span>,<span>,</span>,< span key={"7"} onClick={(e) => {define3(e, "7")}}>-g</span> ];
+  const HTMLCharClasses = [ < span key={"2"} onClick={(e) => {define(e, "2")}}>. </span>, <span>, </span>, < span key={"4"} onClick={(e) => {define(e, "4")}}>\d </span>,<span>, </span>, < span key={"5"} onClick={(e) => {define(e, "5")}}>\D </span>,<span>, </span>, < span key={"7"} onClick={(e) => {define(e, "7")}}>\n </span>,<span>, </span>, < span key={"8"} onClick={(e) => {define(e, "8")}}>\r </span>,<span>, </span>, < span key={"9"} onClick={(e) => {define(e, "9")}}>\s </span>,<span>, </span>, < span key={"10"} onClick={(e) => {define(e, "10")}}>\S </span>,<span>, </span>, < span key={"11"} onClick={(e) => {define(e, "11")}}>\t </span>,<span>, </span>, < span key={"13"} onClick={(e) => {define(e, "13")}}>\w </span>,<span>, </span>, < span key={"14"} onClick={(e) => {define(e, "14")}}>\W </span>,<span>, </span>, < span key={"15"} onClick={(e) => {define(e, "15")}}>\0 </span>,<span>, </span>, < span key={"16"} onClick={(e) => {define(e, "16")}}>[^  ]</span>, <span>,</span>,< span key={"17"} onClick={(e) => {define(e, "17")}}>[x-y]</span> , <span>, </span>, < span key={"1"} onClick={(e) => {define(e, "1")}}>[x|y]</span>]
+  const HTMLAssertions = [< span key={"1"} onClick={(e) => {define2(e, "1")}}>^</span>, <span>, </span>,< span key={"2"}  onClick={(e) => {define2(e, "2")}}>$</span>, <span>, </span>,< span key={"3"}  onClick={(e) => {define2(e, "3")}}>\b</span>, <span>, </span>,< span key={"4"}  onClick={(e) => {define2(e, "4")}}>\B</span>,<span>, </span>, < span key={"5"} onClick={(e) => {define2(e, "5")}}>|</span> ];
+  const HTMLQuantifiers = [< span key={"1"} onClick={(e) => {define3(e, "1")}}>*</span>, <span>, </span>,< span key={"2"} onClick={(e) => {define3(e, "2")}}>+</span>, <span>, </span>,< span key={"3"} onClick={(e) => {define3(e, "3")}}>?</span>, <span>, </span>,< span key={"4"} onClick={(e) => {define3(e, "4")}}>x&#x007B;n&#x007D;</span>, <span>, </span>,< span key={"5"} onClick={(e) => {define3(e, "5")}}>x&#x007B;n,_&#x007D;</span>, <span>, </span>,< span key={"6"} onClick={(e) => {define3(e, "6")}}>x&#x007B;n,m&#x007D;</span>,<span>,</span>,< span key={"7"} onClick={(e) => {define3(e, "7")}}>-g</span> ];
 
 
   const endOfQuery = " :";
@@ -71,18 +71,39 @@ function define (e, index){  //CharClass definitions
   e.preventDefault();
   e.target.style.color = "red";
   document.getElementById("bottomDiv").innerHTML = definitionsHTMLCharClass[index];
+  let enteredExpression = charClasses[index];
+  if((index == "16") || (index == "17") || (index == "1")){
+      enteredExpression = "(" + enteredExpression + ")";
+  }
+  if(translation != null){
+  setTranslation(translation + enteredExpression);
+}else{
+  setTranslation(enteredExpression);
+}
 }
 
 function define2 (e, index){  //Assertions definitions
   e.preventDefault();
   e.target.style.color = "red";
   document.getElementById("bottomDiv").innerHTML = definitionsAssertions[index];
+  let enteredExpression = assertions[index-1];
+  if(translation != null){
+    setTranslation(translation + enteredExpression);
+  }else{
+    setTranslation(enteredExpression);
+  }
 }
 
 function define3 (e, index){  //quantifiers definitions
   e.preventDefault();
   e.target.style.color = "red";
   document.getElementById("bottomDiv").innerHTML = definitionsQuantifiers[index];
+  let enteredExpression = quantifiers[index-1];
+  if(translation != null){
+    setTranslation(translation + enteredExpression);
+  }else{
+    setTranslation(enteredExpression);
+  }
 }
 
 useEffect(() => {
